db21306464fdfdea79570266950e35e7ef04c275
vaishnavinawkar <vaishnavinawkar3101@gmail.com>
2022-01-11 11:02:35 +0530

    DAY8

--- server/API/Orders/index.js
+++ server/API/Orders/index.js
@@ -7,7 +7,7 @@ import passport from "passport";
 import { OrderModel } from "../../database/allModels";
 
 //validateuser
-import validateUser from "../../config/validateUser";
+import ValidateUser from "../../config/validateUser";
 
 const Router = express.Router();
 
@@ -21,7 +21,7 @@ const Router = express.Router();
 
 Router.get("/:_id",passport.authenticate("jwt") ,async(req,res)=> {
     try{
-        await validateUser(req, res)
+        await ValidateUser(req, res);
         const {_id} = req.params;
         const getOrders = await OrderModel.findOne({user: _id});
 
